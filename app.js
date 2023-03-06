4e4664c57c39d91f3c0aa1a004ad7ec2a298d992
Jeremy Lanssiers <jlanssie@gmail.com>
2023-03-06 21:41:03 +0100

    main - update

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let requestCounter = "";
+
+const getDate = () => {
+  return Intl.DateTimeFormat('en-GB', { dateStyle: 'short', timeStyle: 'medium' }).format(Date.now()).replace(",", " -");
+}
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
   const data = req.query.input;
   const processedData = data ? data : "/";
 
-  const payload = "query: " + processedData;
+  requestCounter++;
+
+  const payload = "query: " + processedData + "<br><br>----<br><br>requests received: " + requestCounter;
+
+  console.log(getDate(), "-", req.url, "-", req.body)
 
   res.send(payload);
 });
@@ -17,7 +27,11 @@ app.post("/", (req, res) => {
   const data = req.body;
   const processedData = data ? data : "/";
 
-  const payload = "payload: " + JSON.stringify(processedData);
+  requestCounter++;
+
+  const payload = "payload: " + JSON.stringify(processedData) + " - " + "requests received: " + requestCounter;
+
+  console.log(getDate(), "-", req.url, "-", req.body)
 
   res.send(payload);
 });
